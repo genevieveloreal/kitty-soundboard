67e4515329c9b90b6f60807ab41a75bc4fa39368
genevieveloreal <genevieveloreal@gmail.com>
2018-02-27 23:01:32 +1000

    Added individual cat noises in script

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,23 @@
 var catOne = document.getElementById("catOne"); 
+var catTwo = document.getElementById("catTwo"); 
+var catThree = document.getElementById("catThree"); 
+var catFour = document.getElementById("catFour"); 
 
-function playAudio() { 
+function playOne() { 
     catOne.play(); 
 } 
+
+function playTwo() {
+    catTwo.play();
+}
+
+function playThree() {
+    catThree.play();
+}
+
+function playFour() {
+    catFour.play();
+}
 
 function pauseAudio() { 
     x.pause(); 
