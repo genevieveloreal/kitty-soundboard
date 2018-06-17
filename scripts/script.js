aeed640856706f5e84ea8d20078d811b9d9ff3d1
genevieveloreal <genevieveloreal@gmail.com>
2018-06-17 17:45:48 +1000

    Update of graphics, CSS and audio

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,12 @@ var catOne = document.getElementById("catOne");
 var catTwo = document.getElementById("catTwo"); 
 var catThree = document.getElementById("catThree"); 
 var catFour = document.getElementById("catFour"); 
+var catFive = document.getElementById("catFive"); 
+var catSix = document.getElementById("catSix"); 
+var catSeven = document.getElementById("catSeven"); 
+var catEight = document.getElementById("catEight"); 
+var catNine = document.getElementById("catNine"); 
+var catTen = document.getElementById("catTen"); 
 
 function playOne() { 
     catOne.play(); 
